05255391405ee1356899346732e9f0c518642b98
Magnus Edetorn <magnus.edetorn@tietoevry.com>
2024-02-27 14:43:17 +0100

    LibraryMeta use react-window

--- ClientApp/src/components/LibraryMeta.js
+++ ClientApp/src/components/LibraryMeta.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { FixedSizeList } from 'react-window';
+import AutoSizer from 'react-virtualized-auto-sizer';
 
 const LibraryMeta = () => {
   const [ metaData, setMetadata ] = useState([]);
@@ -13,34 +15,65 @@ const LibraryMeta = () => {
     setMetadata(data);
   }
 
+  const Row = ({ index, style }) => {
+    const isEvenRow = index % 2 === 0;
+    const backgroundColor = isEvenRow ? '#EEE' : '#FFF';
+    const rowStyle = {
+      ...style,
+      backgroundColor,      
+      display: 'flex',
+      justifyContent: 'space-between',
+      padding: '0 16px',
+      wordWrap: 'anywhere'
+    };
+    return (
+      <div style={rowStyle}>
+        <p>{metaData[index].guid}</p>
+        <p>{metaData[index].name}</p>
+        <p>{metaData[index].sizeKb}</p>
+        <p>{metaData[index].dateTaken}</p>
+        <p>{metaData[index].width}</p>
+        <p>{metaData[index].height}</p>
+      </div>
+    );
+  };
+
   return (
-    <div className="white-bkg">
-      <h1 id="tableLabel">Photos metadata</h1>
-      <p>{metaData.length} photos indexed</p>
-      <table className="table table-striped" aria-labelledby="tableLabel">
-        <thead>
-          <tr key="header">
+    <div className="white-bkg" 
+      style={{ display: 'flex', 
+        flexDirection: 'column', 
+        height: '94dvh'}}
+      >
+      <div>
+        <h1>Photos metadata</h1>
+        <p>{metaData.length} photos indexed</p>
+        <div>
+          <thead style={{ display: 'flex', 
+            justifyContent: 'space-between', 
+            padding: '0 16px'}}
+          >
             <th>Guid</th>
             <th>Name</th>
             <th>Size (kB)</th>
             <th>Date Taken</th>
             <th>Width</th>
             <th>Height</th>
-          </tr>
-        </thead>
-        <tbody>
-          {metaData.map(currentMetadata =>
-            <tr key={currentMetadata.guid}>
-              <td>{currentMetadata.guid}</td>
-              <td>{currentMetadata.name}</td>
-              <td>{currentMetadata.sizeKb}</td>
-              <td>{currentMetadata.dateTaken}</td>
-              <td>{currentMetadata.width}</td>
-              <td>{currentMetadata.height}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+          </thead>
+        </div>
+      </div>
+
+      <AutoSizer>
+        {({ height, width }) => (
+          <FixedSizeList 
+            height={height} 
+            width={width} 
+            itemSize={40} 
+            itemCount={metaData.length}
+          >
+            {Row}
+          </FixedSizeList>
+        )}
+      </AutoSizer>
     </div>
   );
 }
